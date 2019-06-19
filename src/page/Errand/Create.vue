cea8a7bc173e03ac427651059d9544f652e2a010
freesdw <571683955@qq.com>
2019-06-19 14:54:12 +0800

    deal with conflict

--- src/page/Errand/Create.vue
+++ src/page/Errand/Create.vue
@@ -1,13 +1,35 @@
 <template>
     <el-row>
-      <el-col :span="24">
-          创建订单
-          <anchored-heading :level="1">Hello world!</anchored-heading>
-          <anchored-heading :level="2">Hello world!</anchored-heading>
-          <anchored-heading :level="3">Hello world!</anchored-heading>
-          <anchored-heading :level="4">Hello world!</anchored-heading>
-          <anchored-heading :level="5">Hello world!</anchored-heading>
-          <anchored-heading :level="6">Hello world!</anchored-heading>
+      <el-col :span="24" class="content-wrapper">
+          <el-form :model="ruleForm" :rules="rules" label-position="left" label-width="80px">
+            <el-form-item label="标题" prop="title">
+              <el-input v-model="ruleForm.title" maxlength="30" show-word-limit></el-input>
+            </el-form-item>
+            <el-form-item label="结束时间" prop="dueDate">
+              <el-date-picker v-model="ruleForm.dueDate" type="datetime"></el-date-picker>
+            </el-form-item>
+            <el-form-item>
+              <el-col :span="11">
+                <el-form-item label="任务报酬(元)" prop="fee" label-width="120px">
+                  <el-input-number v-model="ruleForm.fee" :min="0.01" :max="9999.0" :step="0.01"></el-input-number>
+                </el-form-item>
+              </el-col>
+              <el-col :span="11" :offset="1">
+                <el-form-item label="类型" label-width="100px" prop="tag">
+                  <el-select v-model="ruleForm.tag" placeholder="请选择类型">
+                    <el-option v-for="item in tags" :key="item.value" :label="item.value" :value="item.value">
+                    </el-option>
+                  </el-select>
+                </el-form-item>
+              </el-col>
+            </el-form-item>
+            <el-form-item label="任务描述" prop="description">
+              <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 10 }" v-model="ruleForm.description" maxlength="500" show-word-limit></el-input>
+            </el-form-item>
+            <el-form-item label="" label-width="80px">
+              <el-button type="primary" icon="el-icon-s-claim" @click.prevent="summitForm" style="width:30%;margin-left:calc(30% + 20px)">提交任务</el-button>
+            </el-form-item>
+          </el-form>
       </el-col>
     </el-row>
 </template>
@@ -29,5 +51,56 @@ Vue.component('anchored-heading', {
   }
 })
 export default {
+  data () {
+    return {
+      ruleForm: {
+        title: '',
+        dueDate: '',
+        fee: 0.01,
+        tag: '',
+        description: ''
+      },
+      tags: [{
+        value: '快递'
+      }, {
+        value: '外卖'
+      }],
+      rules: {
+        title: [
+          { required: true, message: '请输入题目', triggr: 'blur' }
+        ],
+        description: [
+          { required: true }
+        ],
+        fee: [
+          { required: true }
+        ],
+        tag: [
+          { required: true }
+        ],
+        dueDate: [
+          { type: 'date', required: true, message: '', triggr: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    summitForm () {
+      if (this.ruleForm.title === '') {
+        this.$message.error('请输入任务标题')
+      } else if (this.ruleForm.dueDate === '') {
+        this.$message.error('请输入任务截止时间')
+      } else if (this.ruleForm.tag === '') {
+        this.$message.error('请选择类型')
+      } else if (this.ruleForm.description === '') {
+        this.$message.error('请增加任务描述')
+      } else {
+        this.$message({
+          message: '提交成功',
+          type: 'success'
+        })
+      }
+    }
+  }
 }
 </script>
--- src/page/Errand/Mine.vue
+++ src/page/Errand/Mine.vue
@@ -1,13 +1,41 @@
 <template>
-    <el-row>
-      <el-col :span="24">
-          我的订单
-          <anchored-heading :level="1">Hello world!</anchored-heading>
-          <anchored-heading :level="2">Hello world!</anchored-heading>
-          <anchored-heading :level="3">Hello world!</anchored-heading>
-          <anchored-heading :level="4">Hello world!</anchored-heading>
-          <anchored-heading :level="5">Hello world!</anchored-heading>
-          <anchored-heading :level="6">Hello world!</anchored-heading>
+    <el-row class="content-warpper">
+      <el-col :span="24" class="toolbar">
+        <el-form :inline="true" :model="filters">
+          <el-form-item>
+            <el-input v-model="filters.title" placeholder="活动标题"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="getFilter">查询</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+      <el-table :data="errandList" highlight-current-row @selection-change="selsChange" style="width: 100%;" stripe>
+        <el-table-column type="selection" width="55">
+        </el-table-column>
+        <el-table-column type="index" width="50"></el-table-column>
+        <el-table-column prop="id" label="ID" width="100" sortable></el-table-column>
+        <el-table-column prop="title" label="活动标题" width="200"></el-table-column>
+        <el-table-column prop="fee" label="报酬" width="80" sortable></el-table-column>
+        <el-table-column prop="finisher" label="接取者" width="100"></el-table-column>
+        <el-table-column prop="dueDate" label="结束日期" width="150"></el-table-column>
+        <el-table-column prop="tag" label="类型" width="100" :filters="[{ text: '快递', value: '快递' }, {text: '外卖', value: '外卖' }]" :filter-method="filterTag" filter-placement="bottom-end">
+          <template slot-scope="scope">
+            <el-tag :type="scope.row.tag === '快递' ? 'primary' : 'success'"
+            disable-transitions>{{ scope.row.tag }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作和描述" width="150">
+          <template slot-scope="scope">
+            <el-tooltip placement="top">
+              <div slot="content">{{ scope.row.description }}</div>
+              <el-button size="small" type="danger" @click="deleteErrand(scope.$index)">删除</el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-col :span="24" class="toolbar">
+        <el-button type="danger" size="medium" style="margin-top: 5px;" @click="batchRemove" :disabled="this.sels.length===0">批量删除</el-button>
       </el-col>
     </el-row>
 </template>
@@ -29,5 +57,71 @@ Vue.component('anchored-heading', {
   }
 })
 export default {
+  data () {
+    return {
+      filters: {
+        title: ''
+      },
+      errandList: [
+        {
+          id: 0,
+          title: '活动1',
+          description: '问卷1描述',
+          fee: 10,
+          dueDate: '1111-1-1',
+          tag: '快递',
+          finisher: '用户1'
+        }, {
+          id: 1,
+          title: '轰动2',
+          description: '问卷2描述',
+          fee: 10,
+          dueDate: '1111-1-1',
+          tag: '外卖',
+          finisher: '用户1'
+        },
+        {
+          id: 3,
+          title: '问卷3',
+          description: '问卷3描述',
+          fee: 50,
+          dueDate: '1131-1-1',
+          tag: '外卖',
+          finisher: '用户1'
+        },
+        {
+          id: 4,
+          title: '问卷4',
+          description: '问卷4描述',
+          fee: 0.3,
+          dueDate: '1131-1-1',
+          tag: '快递',
+          finisher: '无'
+        }
+      ],
+      sels: []
+    }
+  },
+  methods: {
+    getFilter () {
+      // 从后端根据查询条件获取对应的活动，放在errandList中
+      alert('查询条件' + this.filters)
+    },
+    selsChange: function (sels) {
+      this.sels = sels
+    },
+    filterTag (value, row) {
+      return row.tag === value
+    },
+    batchRemove () {
+      // 根据sels中的下标，找到对应问卷的id，传给后端进行删除，返回成功再在前端questionnaireList进行删除
+    },
+    deleteErrand (index) {
+      // 根据this.errandList[index].id 想后端请求删除的活动，如果返回success，在前端的
+      // this.errandList删除对应活动
+      event.cancelBubble = true
+      alert('删除问卷' + this.errandList[index].title)
+    }
+  }
 }
 </script>
