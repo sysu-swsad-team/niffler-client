a9e493e94b3a5722ae70370821052ebf2c20cb12
freesdw <571683955@qq.com>
2019-06-22 15:38:54 +0800

    merge

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,14 @@
 import axios from 'axios'
 
+// config
 // let base = 'http://127.0.0.1:8080'
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
 
+// user info
 export const postLogin = params => {
   return axios.post('/questionnaire/login/', params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
-}
-
-export const deleteQN = params => {
-  return axios.post('/questionnaire/mine/delete/', params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
 }
 
 export const postRegister = params => {
@@ -25,6 +23,7 @@ export const postVercode = params => {
   return axios.post('/questionnaire/register/vercode/', params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
 }
 
+// questionnaire
 export const summitQN = params => {
   return axios.post('questionnaire/create/', params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
 }
@@ -33,12 +32,33 @@ export const getAllQN = params => {
   return axios.get('questionnaire/', params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
 }
 
+export const getAllQNFilter = params => {
+  return axios.get('questionnaire/filter/', params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+}
+
+export const getQNDetail = params => {
+  return axios.get('questionnaire/detail/', params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+}
+
 export const getMyQN = params => {
   return axios.get('questionnaire/mine/', params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
 }
 
 export const getMyQNFilter = params => {
-  return axios.get('questionnaire/mine/filter', params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+  return axios.get('questionnaire/mine/filter/', params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+}
+
+export const deleteQN = params => {
+  return axios.post('/questionnaire/mine/delete/', params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+}
+
+export const deleteBatchQN = params => {
+  return axios.post('questionnaire/mine/batchDelete/', params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+}
+
+// errand
+export const summitErrand = params => {
+  return axios.post('errand/create/', params, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
 }
 
 // export const getUserList = params => { return axios.get(`${base}/user/list`, { params: params }) }
