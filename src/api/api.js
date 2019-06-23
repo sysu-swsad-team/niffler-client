09c8126b7c29094be4e65eeada2f5ed61da84b8d
wuzht <770333760@qq.com>
2019-06-23 15:42:12 +0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
+axios.defaults.withCredentials = true
 
 const header = {headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
 
