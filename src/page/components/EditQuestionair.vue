e7c29c0427d2e52945ee6052035e23cc5397f650
freesdw <571683955@qq.com>
2019-06-26 20:45:26 +0800

    fix bugs

--- src/page/components/EditQuestionair.vue
+++ src/page/components/EditQuestionair.vue
@@ -291,9 +291,9 @@ export default {
               title: this.ruleForm.title,
               tag: [this.ruleForm.tag],
               description: this.ruleForm.description,
-              maxNumber: this.ruleForm.maxNumber,
+              maxNumber: this.ruleForm.maxNumber, 
               fee: this.ruleForm.fee,
-              dueDate: this.ruleForm.dueDate,
+              dueDate: this.ruleForm.dueDate.toString(),
               questions: this.ruleForm.questions
             }
             summitQN(summitParams).then(res => {
