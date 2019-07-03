231f5fabf418f06f55649f8d3789d2d63f596081
wuzht <770333760@qq.com>
2019-07-03 14:16:37 +0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -27,10 +27,10 @@ export default {
   clearCookies: function () {
     var date = new Date()
     date.setTime(date.getTime() - 10000)
-    var keys = document.cookie.match(/[^ =;]+(?=\=)/g)
+    var keys = document.cookie.match(/[^ =;]+(?==)/g)
     if (keys) {
       for (var i = keys.length; i--;) {
-        document.cookie = keys[i] + "=0; expire=" + date.toGMTString() + "; path=/"
+        document.cookie = keys[i] + '=0; expire=' + date.toGMTString() + '; path=/'
       }
     }
   }
